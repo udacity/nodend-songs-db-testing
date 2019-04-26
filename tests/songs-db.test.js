2add4351a13e83a3a752fae07d39501ebb5c9301
Julian Duque <julianduquej@gmail.com>
2019-04-26 10:09:08 -0700

    Add tests for createSong

--- tests/songs-db.test.js
+++ tests/songs-db.test.js
@@ -52,3 +52,53 @@ test('songs-db: list songs by artist - not existant', async t => {
   }
   t.end()
 })
+
+test('songs-db: create a song', async t => {
+  // Setup
+  await setup()
+
+  // Teardown
+  t.on('end', () => {
+    wipeDatabase()
+  })
+
+  // Test
+  try {
+    const db = await SongsDb.at()
+    const result = await db.createSong({
+      artist: 'Dio',
+      album: 'Dio',
+      song: 'Rainbow in the Dark'
+    })
+    t.ok(result, 'should not be undefined')
+    t.equal(typeof result, 'object', 'should be an object')
+    t.ok(result.id.length > 5, 'should have a valid id')
+  } catch (err) {
+    t.error(err, 'should not fail')
+  }
+  t.end()
+})
+
+test('songs-db: create a song - failure', async t => {
+  // Setup
+  await setup()
+
+  // Teardown
+  t.on('end', () => {
+    wipeDatabase()
+  })
+
+  // Test
+  try {
+    const db = await SongsDb.at()
+    await db.createSong({
+      artist: 'Dio',
+      album: 'Dio'
+    })
+    t.fail('should throw an error')
+  } catch (err) {
+    t.ok(err, 'should throw an error')
+    t.ok(err.message.match(/cannot be null/), 'with meaninful message')
+  }
+  t.end()
+})
